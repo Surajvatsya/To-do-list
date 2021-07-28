45fe3db2b0ce4a07bf4bf150070f30ca96c18e1f
Suraj Vatsya <surajkumar742494@gmail.com>
2021-07-29 01:14:50 +0530

    database hosted on cloud

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://suraj_admin:suraj@cluster0.loyaa.mongodb.net/todolistDB", {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
